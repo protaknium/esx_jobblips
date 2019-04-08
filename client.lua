d3031b5137f4bbdb35a54218bf073104e4ed69cf
protaknium <49377950+protaknium@users.noreply.github.com>
2019-04-08 10:34:13 +0300

    Removed 1,

--- client.lua
+++ client.lua
@@ -2,7 +2,7 @@ ESX = nil local PlayerData = {} Citizen.CreateThread(function() while ESX == nil
 
 RegisterNetEvent('esx:playerLoaded') AddEventHandler('esx:playerLoaded', function(xPlayer) PlayerData = xPlayer end) RegisterNetEvent('esx:setJob') AddEventHandler('esx:setJob', function(job) PlayerData.job = job end)
 
-local policeblips = { {title="Get medical cannabis here", colour=25, blip=140, x = 1525.29, y=1710.02, z=109.00},, }
+local policeblips = { {title="Get medical cannabis here", colour=25, blip=140, x = 1525.29, y=1710.02, z=109.00}, }
 
 local ambulanceblips = { {title="Get medical cannabis here", colour=25, blip=140, x = 1525.29, y=1710.02, z=109.00}, }
 
