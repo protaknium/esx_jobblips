d1135a0d42f9ebfae685f95e3c91906108487f5d
protaknium <49377950+protaknium@users.noreply.github.com>
2019-04-09 18:41:21 +0300

    Full rework made!
    Thanks to Epriih for helping me out on this one <3

--- client.lua
+++ client.lua
@@ -1,12 +1,66 @@
-ESX = nil local PlayerData = {} Citizen.CreateThread(function() while ESX == nil do TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end) Citizen.Wait(0) end end)
+PlayerData = {}
 
-RegisterNetEvent('esx:playerLoaded') AddEventHandler('esx:playerLoaded', function(xPlayer) PlayerData = xPlayer end) RegisterNetEvent('esx:setJob') AddEventHandler('esx:setJob', function(job) PlayerData.job = job end)
+local done = false
 
-local policeblips = { {title="Get medical cannabis here", colour=25, blip=140, x = 1525.29, y=1710.02, z=109.00}, }
+ESX = nil
+ Citizen.CreateThread(function()
+    while ESX == nil do
+        TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+        Citizen.Wait(0)
+    end
+    while ESX.GetPlayerData().job == nil do
+        Citizen.Wait(10)
+    end
+    PlayerData = ESX.GetPlayerData()
+    done = true
+end)
+--==============ESX^^^^^^No TOUCHING!================---
 
-local ambulanceblips = { {title="Get medical cannabis here", colour=25, blip=140, x = 1525.29, y=1710.02, z=109.00}, }
+local policeblips = { 
+	 {title="Example", colour=25, id=140, x = 1525.29, y= 1710.02, z= 109.00}, 
+}
 
+local ambulanceblips = { 
+	 {title="Example", colour=25, id=140, x = 1525.29, y= 1710.02, z= 109.00}, 
+}
+--Police blips
+ Citizen.CreateThread(function() 
+    while not done do
+        Citizen.Wait(10)
+    end
+    if PlayerData.job.name == 'police' then 
+        for _, info in pairs(policeblips) do 
+        info.blip = AddBlipForCoord(info.x, info.y, info.z)
+      SetBlipSprite(info.blip, info.id)
+      SetBlipDisplay(info.blip, 4)
+      SetBlipScale(info.blip, 1.0)
+      SetBlipColour(info.blip, info.colour)
+      SetBlipAsShortRange(info.blip, false)
+      BeginTextCommandSetBlipName("STRING")
+      AddTextComponentString(info.title)
+      EndTextCommandSetBlipName(info.blip)
+      done = true
+    end
+end
+end)
 
-Citizen.CreateThread(function() if PlayerData.job.name == 'police' then for _, info in pairs(policeblips) do info.blip = AddBlipForCoord(info.x, info.y, info.z) SetBlipSprite(info.blip, info.id) SetBlipDisplay(info.blip, 4) SetBlipScale(info.blip, 0.9) SetBlipColour(info.blip, info.colour) SetBlipAsShortRange(info.blip, true) BeginTextCommandSetBlipName("STRING") AddTextComponentString(info.title) EndTextCommandSetBlipName(info.blip) end end 
-
-if PlayerData.job.name == 'ambulance' then for _, info in pairs(ambulanceblips) do info.blip = AddBlipForCoord(info.x, info.y, info.z) SetBlipSprite(info.blip, info.id) SetBlipDisplay(info.blip, 4) SetBlipScale(info.blip, 0.9) SetBlipColour(info.blip, info.colour) SetBlipAsShortRange(info.blip, true) BeginTextCommandSetBlipName("STRING") AddTextComponentString(info.title) EndTextCommandSetBlipName(info.blip) end end end)
+-- Ambulance blips
+ Citizen.CreateThread(function() 
+    while not done do
+        Citizen.Wait(10)
+    end
+    if PlayerData.job.name == 'ambulance' then 
+        for _, info in pairs(ambulance) do 
+        info.blip = AddBlipForCoord(info.x, info.y, info.z)
+      SetBlipSprite(info.blip, info.id)
+      SetBlipDisplay(info.blip, 4)
+      SetBlipScale(info.blip, 1.0)
+      SetBlipColour(info.blip, info.colour)
+      SetBlipAsShortRange(info.blip, false)
+      BeginTextCommandSetBlipName("STRING")
+      AddTextComponentString(info.title)
+      EndTextCommandSetBlipName(info.blip)
+      done = true
+    end
+end
+end)
